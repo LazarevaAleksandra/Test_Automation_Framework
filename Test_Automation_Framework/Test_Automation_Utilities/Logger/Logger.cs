9aba0ea1b4c3d8e7ced1a497dcbfb900e452f298
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-12-25 14:14:31 +0300

    start test_automation_framework

---
+++ Test_Automation_Framework/Test_Automation_Utilities/Logger/Logger.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+
+namespace Epam_TestAutomation_Utilities.Logger
+{
+    public static class Logger
+    {
+        public static ThreadLocal<List<string>> _logger = 
+            new ThreadLocal<List<string>>(() => new List<string>());
+
+        public static void Info(string message)
+        {
+            _logger.Value.Add(message);
+        }
+
+        public static void InitLogger(string filePath)
+        {
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug().WriteTo.File(Path.Combine(filePath, "logs.txt"))
+                .CreateLogger();
+        }
+
+        public static void FinishTestLog()
+        {
+            _logger.Value.ForEach(x => Log.Logger.Information(x));
+            _logger.Value.Clear();
+        }
+    }
+}
