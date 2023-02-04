6c01fe59c59a6127437d197bcf336043617b3387
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2023-02-04 15:19:00 +0300

    fix comments

--- Test_Automation_Framework/Epam_TestAutomation_API/Controllers/BiblesController.cs
+++ Test_Automation_Framework/Epam_TestAutomation_API/Controllers/BiblesController.cs
@@ -2,6 +2,9 @@
 
 namespace Epam_TestAutomation_API.Controllers
 {
+    /// <summary>
+    /// Class that contains methods and resources for /v1 micro service
+    /// </summary>
     public class BiblesController : BaseController
     {
         public BiblesController(CustomRestClient client, string apiKey = "") : base(client, Service.Bibles, apiKey) {}
@@ -12,16 +15,31 @@ namespace Epam_TestAutomation_API.Controllers
         private const string AudioBiblesResource = "/v1/audio-bibles";
         private const string SingleAudioBiblesResource = "/v1/audio-bibles/{0}";
 
+        /// <summary>
+        /// Request that receive all list of bibles
+        /// </summary>
+        /// <typeparam name="T"><see cref="AllBiblesModels"/></typeparam>
+        /// <returns>response typeof <see cref="RestResponse"/> and <see cref="AllBiblesModels"/></returns>
         public (RestResponse Response, T? Bibles) GetBibles<T>()
         {
             return Get<T>(BiblesResource);
         }
 
+        /// <summary>
+        /// Request that receive all list of audio bibles
+        /// </summary>
+        /// <typeparam name="T"><see cref="AllBiblesModels"/></typeparam>
+        /// <returns>response typeof <see cref="RestResponse"/> and <see cref="AllBiblesModels"/></returns>
         public (RestResponse Response, T? Bibles) GetAudioBibles<T>()
         {
             return Get<T>(AudioBiblesResource);
         }
 
+        /// <summary>
+        /// Request that receive audio bible
+        /// </summary>
+        /// <typeparam name="T"><see cref="AllBiblesModels"/></typeparam>
+        /// <returns>response typeof <see cref="RestResponse"/> and <see cref="AllBiblesModels"/></returns>
         public (RestResponse Response, T? Bibles) GetAudioBible<T>(string id)
         {
             return Get<T>(string.Format(SingleAudioBiblesResource, id));
--- Test_Automation_Framework/Epam_TestAutomation_API/Controllers/PhoneController.cs
+++ Test_Automation_Framework/Epam_TestAutomation_API/Controllers/PhoneController.cs
@@ -3,6 +3,9 @@ using RestSharp;
 
 namespace Epam_TestAutomation_API.Controllers
 {
+    /// <summary>
+    /// Class that contains methods and resources for /objects micro service
+    /// </summary>
     public class PhoneController : BaseController
     {
         public PhoneController(CustomRestClient client) : base(client, Service.Phones) {}
@@ -10,11 +13,23 @@ namespace Epam_TestAutomation_API.Controllers
         private const string PhonesResource = "/objects";
         private const string SinglePhonesResourceID = "/objects?id={0}";
 
+
+        /// <summary>
+        /// Request that receive all list of phones
+        /// </summary>
+        /// <typeparam name="T"><see cref="AllPhonesModels"/></typeparam>
+        /// <returns>response typeof <see cref="RestResponse"/> and <see cref="AllPhonesModels"/></returns>
         public (RestResponse Response, T? Phones) GetPhones<T>()
         {
             return Get<T>(PhonesResource);
         }
 
+
+        /// <summary>
+        /// Request that receive phone by id
+        /// </summary>
+        /// <typeparam name="T"><see cref="AllPhonesModels"/></typeparam>
+        /// <returns>response typeof <see cref="RestResponse"/> and <see cref="AllPhonesModels"/></returns>
         public (RestResponse Response, T? Phone) GetPhonesID<T>(string id)
         {
             return Get<T>(string.Format(SinglePhonesResourceID, id));
